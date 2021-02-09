dbb53824b27c955ac5eb9ee4ea8ab817157eec0e
Marcos Paulo <marcospsilva222@hotmail.com>
2021-02-09 08:28:26 -0300

    Meu perfil pessoal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+
+import { TempladeModule } from './core/templade/templade.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  BrowserModule,
+    AppRoutingModule,
+    TempladeModule,
+    NgbModule,
+    NgxSpinnerModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/templade/templade.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+
+@NgModule({
+  declarations: [FooterComponent, NavbarComponent],
+  imports: [
+CommonModule,
+NgbModule
+  ],
+  exports: [FooterComponent, NavbarComponent]
+})
+export class TempladeModule { }
